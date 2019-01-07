e95cbf5589a3b9f53af097af67b3225e5a18ee2f
Jonas Norling <jonas.norling@gmail.com>
2019-01-07 21:36:23 +0100

    RRD saver from MQTT

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Publish one-wire sensor values with MQTT
+## mqtt_1w.py: Publish one-wire sensor values with MQTT
 
 This script is intended to be run on a Raspberry Pi to publish temperature sensor readings to MQTT. It should be portable to any platform where /sys/bus/w1 is available (CONFIG_W1 and CONFIG_W1_MASTER_GPIO).
 
@@ -6,11 +6,17 @@ Dependencies:
 
 - sudo pip3 install paho-mqtt
 
-
-## Running on Raspberry Pi
+### Running on Raspberry Pi
 
 One-wire must be enabled on the Raspberry Pi, either by enabling the appropriate option with `raspi-config` or by adding `dtoverlay=w1-gpio` to `/boot/config.txt`. When properly enabled, any one-wire devices should appear as `/sys/bus/w1/devices/*/w1_slave`.
 
 See https://pinout.xyz/pinout/1_wire.
 
 This script can be run as a service from systemd. See mqtt_1w.service for installation instructions.
+
+
+## mqtt_to_rrd.py: Save sensor values to RRD from MQTT
+
+Dependencies:
+
+- sudo apt-get install rrdtool
---
+++ mqtt_to_rrd.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+#
+# Pull temperature readings from MQTT and add to an RRD
+#
+
+import argparse
+import logging
+import platform
+import paho.mqtt.client as mqtt
+import time
+import re
+import subprocess
+from pathlib import Path
+
+TOPIC_MATCH = "temperature/#"
+TOPIC_RE = re.compile(TOPIC_MATCH.replace("#", "(.+)"))
+
+rrd_path = None
+
+def create_rrd(rrdfile):
+    try:
+        log.info("Creating RRD %s" % rrdfile)
+        # 4 weeks with minute level data
+        HIGH_RES_SAMPLES = 40320
+        # 365 days with ten minute level data
+        MED_RES_SAMPLES = 52560
+        # ten years with hour level data
+        LOW_RES_SAMPLES = 87600
+        completed = subprocess.run(["rrdtool", "create", str(rrdfile),
+                "-O", "--step", "60",
+                "DS:value:GAUGE:120:-60:60",
+                "RRA:AVERAGE:0.5:1:%d" % HIGH_RES_SAMPLES,
+                "RRA:AVERAGE:0.5:10:%d" % MED_RES_SAMPLES,
+                "RRA:AVERAGE:0.5:60:%d" % LOW_RES_SAMPLES,
+                "RRA:MAX:0.5:1:%d" % HIGH_RES_SAMPLES,
+                "RRA:MAX:0.5:10:%d" % MED_RES_SAMPLES,
+                "RRA:MAX:0.5:60:%d" % LOW_RES_SAMPLES,
+                "RRA:MIN:0.5:1:%d" % HIGH_RES_SAMPLES,
+                "RRA:MIN:0.5:10:%d" % MED_RES_SAMPLES,
+                "RRA:MIN:0.5:60:%d" % LOW_RES_SAMPLES])
+        if completed.returncode != 0:
+            log.error("RRD create failed")
+    except FileNotFoundError as e:
+        log.error(e)
+
+def update_rrd(timestamp, sensorname, value):
+    log.debug("Reading at %d for %s: %.3f" % (timestamp, sensorname, value))
+    rrdfile = Path(rrd_path, "%s.rrd" % sensorname)
+    if not rrdfile.is_file():
+        create_rrd(str(rrdfile))
+    try:
+        completed = subprocess.run(["rrdtool", "update", str(rrdfile),
+                "%d:%f" % (timestamp, value)])
+        if completed.returncode != 0:
+            log.error("RRD update failed")
+    except FileNotFoundError as e:
+        log.error(e)
+
+def on_connect(client, userdata, flags, rc):
+    log.info("Connected: %s" % rc)
+
+    client.subscribe("$SYS/broker/version")
+    client.subscribe(TOPIC_MATCH)
+
+def on_message(client, userdata, msg):
+    log.debug("Message: %s %s" % (msg.topic, msg.payload))
+    match = TOPIC_RE.match(msg.topic)
+    if match:
+        try:
+            sensorname = match.group(1)
+            value = float(msg.payload)
+        except ValueError:
+            log.warning("Bad payload: %s %s"% (msg.topic, msg.payload))
+            return
+
+        update_rrd(int(time.time()), sensorname, value)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    log = logging.getLogger("mqtt_to_rrd")
+
+    parser = argparse.ArgumentParser(description="Pull temperature readings from MQTT and add to an RRD")
+    parser.add_argument("--mqtt", metavar="ADDRESS", default="localhost",
+                        help="MQTT broker address")
+    parser.add_argument("--rrd-path", metavar="PATH", default=".",
+                        help="Path to directory with RRD files")
+    parser.add_argument("--debug", default=False, action="store_true",
+                        help="Enable debug printouts")
+    args = parser.parse_args()
+    
+    if args.debug:
+        logging.getLogger().setLevel('DEBUG')
+
+    client_id = "%s-%s" % ("mqtt_to_rrd", platform.node())
+    rrd_path = args.rrd_path
+
+    client = mqtt.Client(client_id=client_id, clean_session=True)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(args.mqtt)
+    log.info("Connected as %s" % client_id)
+    
+    try:
+        client.loop_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        client.disconnect()
