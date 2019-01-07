134aa0a26dce9af189a97237b50fcf4dfed813e8
Jonas Norling <jonas.norling@gmail.com>
2019-01-07 20:43:38 +0100

    Publish one-wire sensor values with MQTT

---
+++ README.md
@@ -0,0 +1,14 @@
+# Publish one-wire sensor values with MQTT
+
+This script is intended to be run on a Raspberry Pi to publish temperature sensor readings to MQTT. It should be portable to any platform where /sys/bus/w1 is available (CONFIG_W1 and CONFIG_W1_MASTER_GPIO).
+
+Dependencies:
+
+- sudo pip3 install paho-mqtt
+
+
+## Running on Raspberry Pi
+
+One-wire must be enabled on the Raspberry Pi, either by enabling the appropriate option with `raspi-config` or by adding `dtoverlay=w1-gpio` to `/boot/config.txt`. When properly enabled, any one-wire devices should appear as `/sys/bus/w1/devices/*/w1_slave`.
+
+See https://pinout.xyz/pinout/1_wire.
---
+++ mqtt_1w.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+#
+# Publish one-wire sensor values with MQTT
+#
+
+import argparse
+import logging
+import platform
+import paho.mqtt.client as mqtt
+from pathlib import Path
+import time
+
+TOPIC = "temperature/%s"
+W1_PATH = "/sys/bus/w1/devices/"
+
+def on_connect(mqtt_client, userdata, flags, rc):
+    log.info("Connected: %s" % rc)
+
+def sample_loop(t, mqtt_client):
+    last_time = time.time() - t
+    while True:
+        t = args.time
+        try:
+            time.sleep(last_time + t - time.time())
+        except ValueError:
+            pass
+
+        last_time = time.time()
+        sample_onewire(mqtt_client)
+
+def parse(lines):
+    if lines[0].strip()[-3:] != 'YES':
+        raise RuntimeError("Bad CRC")
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        t = float(temp_string) / 1000.0
+        if t == 85.000:
+            raise RuntimeError("Missing temperature reading")
+        return t
+    else:
+        raise RuntimeError("Unable to parse")
+
+def sample_onewire(mqtt_client):
+    log.debug("Sampling one-wire sensors")
+
+    for s_path in Path(W1_PATH).glob("*/w1_slave"):
+        sensor_name = s_path.parts[-2]
+        log.debug("Reading sensor %s" % sensor_name)
+        try:
+            text = s_path.read_text()
+            temperature = parse(text.splitlines())
+            log.debug("Sensor %s = %.3f C" % (sensor_name, temperature))
+            mqtt_client.publish(TOPIC % sensor_name, temperature)
+        except (IOError, RuntimeError) as e:
+            log.warning(e)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    log = logging.getLogger("mqtt_1w")
+
+    parser = argparse.ArgumentParser(description="Publish one-wire sensor values with MQTT")
+    parser.add_argument("--mqtt", metavar="ADDRESS", default="localhost",
+                        help="MQTT broker address")
+    parser.add_argument("--time", metavar="SEC", default=20, type=int,
+                        help="Sample interval [seconds]")
+    parser.add_argument("--debug", default=False, action="store_true",
+                        help="Enable debug printouts")
+    args = parser.parse_args()
+    
+    if args.debug:
+        logging.getLogger().setLevel('DEBUG')
+
+    client_id = "%s-%s" % ("1w", platform.node())
+    client = mqtt.Client(client_id=client_id)
+    client.on_connect = on_connect
+    client.connect(args.mqtt)
+
+    try:
+        client.loop_start()
+        sample_loop(args.time, client)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        client.loop_stop()
+        client.disconnect()
