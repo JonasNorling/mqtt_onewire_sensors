7383ecd5cb2bd60e9ebc0752f5d7e7860ddbf591
Jonas Norling <jonas@elemental.se>
2020-07-06 14:08:09 +0200

    Plot

--- plot_sql.py
+++ plot_sql.py
@@ -23,36 +23,55 @@ def get_series_id(db: sqlite3.Connection, name: str) -> int:
         return result[0]
 
 
-def plot(db: sqlite3.Connection, series_and_labels: List, start_time: int, end_time: int, step: int):
-    #log.info(mpl.style.available)
+def plot_series(db: sqlite3.Connection, ax, series_name: str, label: str, start_time: int, end_time: int):
+    series_id = get_series_id(db, series_name)
+    if series_id is None:
+        log.error(f'No such series {series_name}')
+        return
+    cur = db.execute(f'SELECT (time/60*60) as time, value AS "{series_name}" FROM samples '
+                     'WHERE series=? AND time >= ? AND time <= ? ORDER BY time',
+                     (series_id, start_time - MAX_FORWARD_FILL, end_time + MAX_FORWARD_FILL))
+    data: List[Tuple[int, float]] = list(cur)
+    last_time = None
+    # Insert dummy samples to inhibit lines that are too long
+    for i, (t, v) in enumerate(data):
+        if last_time is not None and t > last_time + MAX_FORWARD_FILL:
+            data.insert(i, (t-1, None))
+        last_time = t
+    ax.plot(mpl.dates.epoch2num([t for t, v in data]), [v for t, v in data],
+            '-', label=label)
+
+def plot(args):
+    series_and_labels = list(itertools.zip_longest(args.series, args.l, fillvalue="sensor"))
+
+    db = sqlite3.connect(f'file:{args.db}?mode=ro', uri=True)
+    end_time = int(time.time() // 60) * 60
+    start_time = end_time - args.time
+
+    hour_count = (end_time - start_time) // (60 * 60)
+    day_count = hour_count // 24
+
     mpl.style.use('dark_background')
     fig, ax = plt.subplots(facecolor='#332222', figsize=(10, 4))
     ax.set_facecolor('#332222')
-    ax.xaxis.set_major_locator(mpl.dates.HourLocator())
-    ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%H'))
+    if hour_count < 48:
+        ax.xaxis.set_major_locator(mpl.dates.HourLocator())
+        ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%H'))
+    elif day_count <= 7:
+        ax.xaxis.set_major_locator(mpl.dates.DayLocator())
+        ax.xaxis.set_minor_locator(mpl.dates.HourLocator(interval=6))
+        ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%a'))
+    else:
+        ax.xaxis.set_major_locator(mpl.dates.MonthLocator())
+        ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%b'))
     ax.set_xlim(mpl.dates.epoch2num(start_time), mpl.dates.epoch2num(end_time))
     ax.grid(color='#444444')
 
     for series_name, label in series_and_labels:
-        series_id = get_series_id(db, series_name)
-        if series_id is None:
-            log.error(f'No such series {series_name}')
-            continue
-        cur = db.execute(f'SELECT (time/60*60) as time, value AS "{series_name}" FROM samples '
-                         'WHERE series=? AND time >= ? AND time <= ? ORDER BY time',
-                         (series_id, start_time - MAX_FORWARD_FILL, end_time + MAX_FORWARD_FILL))
-        data: List[Tuple[int, float]] = list(cur)
-        last_time = None
-        # Insert dummy samples to inhibit lines that are too long
-        for i, (t, v) in enumerate(data):
-            if last_time is not None and t > last_time + MAX_FORWARD_FILL:
-                data.insert(i, (t-1, None))
-            last_time = t
-        ax.plot(mpl.dates.epoch2num([t for t, v in data]), [v for t, v in data],
-                '-', label=label)
+        plot_series(db, ax, series_name, label, start_time, end_time)
 
-    ax.legend()
-    plt.savefig('plot.png')
+    fig.legend(loc='upper center', ncol=len(series_and_labels), frameon=False)
+    plt.savefig(args.out)
 
 
 if __name__ == '__main__':
@@ -61,10 +80,11 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Plot readings from a SQL database")
     parser.add_argument(nargs="+", dest="series", metavar="SERIES", help="Data series")
-    parser.add_argument("-l", action="append", help="Data label (can be repeated)")
+    parser.add_argument("-l", metavar="LABEL", action="append", help="Data label (can be repeated)")
     parser.add_argument("--db", metavar="FILE", default="samples.sqlite", help="SQLite database file")
-    parser.add_argument("--debug", default=False, action="store_true",
-                        help="Enable debug printouts")
+    parser.add_argument("--time", metavar="SECONDS", type=int, default=24*60*60, help="Time span to plot, in seconds")
+    parser.add_argument("--out", metavar="FILENAME", default="plot.png", help="Output filename")
+    parser.add_argument("--debug", default=False, action="store_true", help="Enable debug printouts")
     args = parser.parse_args()
     
     if args.debug:
@@ -72,10 +92,5 @@ if __name__ == '__main__':
 
     if args.l is None:
         args.l = []
-    series_and_labels = list(itertools.zip_longest(args.series, args.l, fillvalue="sensor"))
 
-    db = sqlite3.connect(f'file:{args.db}?mode=ro', uri=True)
-    end_time = int(time.time() // 60) * 60
-    start_time = end_time - 24 * 60 * 60
-    step = 60
-    plot(db, series_and_labels, start_time, end_time, step)
+    plot(args)
