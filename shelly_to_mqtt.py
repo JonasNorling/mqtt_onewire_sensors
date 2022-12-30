0363ea70017744b9eeb3550554332900b3a516c9
Jonas Norling <jonas@elemental.se>
2022-12-30 09:46:21 +0100

    Add MQTT to Shelly script

--- shelly_to_mqtt.py
+++ shelly_to_mqtt.py
@@ -2,13 +2,17 @@
 #
 # Receive sensor updates from Shelly H&T
 #
-
+import argparse
 import logging
 import math
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import platform
 from urllib.parse import urlparse, parse_qs
 
-log = logging.getLogger("shelly_to_mqtt")
+from mqtt_connection import MqttConnection
+
+log = logging.getLogger('shelly_to_mqtt')
+mqtt_connection: MqttConnection = None
 
 
 class RequestHandler(BaseHTTPRequestHandler):
@@ -21,6 +25,8 @@ class RequestHandler(BaseHTTPRequestHandler):
         temp = float(qs.get('temp', [math.nan])[0])
         sensor_id = qs.get('id', [''])[0]
         log.info(f'Report from {sensor_id}: temp={temp:.2f}°C RH={hum:.0f}%')
+        mqtt_connection.publish(f'temperature/{sensor_id}', str(temp))
+        mqtt_connection.publish(f'humidity/{sensor_id}', str(hum))
         self.send_response(200)
         self.end_headers()
 
@@ -30,16 +36,39 @@ class RequestHandler(BaseHTTPRequestHandler):
         log.debug("%s - - [%s] %s" %
                   (self.address_string(),
                    self.log_date_time_string(),
-                   message.translate(self._control_char_table)))
+                   message))
 
 
-def run_server():
-    server_address = ('', 7123)
-    httpd = HTTPServer(server_address, RequestHandler)
-    log.info(f'Listening on {server_address}')
+def run_http_server(port):
+    httpd = HTTPServer(('', port), RequestHandler)
+    log.info(f'Listening on port {port}')
     httpd.serve_forever()
 
 
-if __name__ == "__main__":
+def run():
     logging.basicConfig(level=logging.INFO)
-    run_server()
+
+    parser = argparse.ArgumentParser(description='Publish Shelly H&T sensors on MQTT')
+    parser.add_argument('--port', default=7123, type=int, help='HTTP server port')
+    parser.add_argument('--debug', default=False, action='store_true',
+                        help='Enable debug printouts')
+    MqttConnection.add_args(parser)
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.getLogger().setLevel('DEBUG')
+
+    global mqtt_connection
+    mqtt_connection = MqttConnection(f'shelly-{platform.node()}', args, log)
+
+    try:
+        mqtt_connection.start()
+        run_http_server(args.port)
+    except KeyboardInterrupt:
+        log.info('Exit on CTRL-C')
+    finally:
+        mqtt_connection.stop()
+
+
+if __name__ == "__main__":
+    run()
